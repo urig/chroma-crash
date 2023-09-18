5a9af245cad652cd03bb3da8c173464363c6cb4a
urig <urig@users.noreply.github.com>
2023-09-18 11:34:52 +0300

    Local logging

--- chroma_crash/app.py
+++ chroma_crash/app.py
@@ -1,8 +1,16 @@
+import logging
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    import chromadb
+    logging.basicConfig()
+    try:
+        import chromadb
+    except Exception as ex:
+        logging.exception("importing chromadb failed")
+        return str(ex)
+    logging.info("importing chromadb succeeded")
     return "<p>Hello, World!</p>"
